e7669aa7cbebe8c9c33a2f6f9861039d74528bb1
Anila1997 <85822563+Anila1997@users.noreply.github.com>
2021-06-28 17:36:02 +0530

    Add files via upload

---
+++ day1/College.java
@@ -0,0 +1,24 @@
+package week3.day2;
+
+public class College  extends University{
+	
+	
+		
+	
+	
+
+
+	@Override
+	public  void ug() {
+		// TODO Auto-generated method stub
+		System.out.println("iam undergraduated");	
+	}
+	public static void main(String[] args) {
+		College newClg = new College();
+		newClg.pg();
+		newClg.ug();
+	}
+	
+		// TODO Auto-generated method stub
+		
+	}
