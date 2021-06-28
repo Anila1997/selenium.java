813d4083937ccffa55dfb92b08a8bd37c5985eed
Anila1997 <85822563+Anila1997@users.noreply.github.com>
2021-06-28 17:16:08 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class College {
+public void collegeName() {
+	System.out.println("college name is sreepathy eng college");
+	}
+public void collegeCode() {
+	System.out.println("SPT");
+}
+public void collegeRank() {
+	System.out.println("college rank is 4");
+}
+}
