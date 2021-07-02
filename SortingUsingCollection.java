e7fd6e6601421cc937729f18ac3ba3d546c5f3c0
Anila1997 <85822563+Anila1997@users.noreply.github.com>
2021-07-02 21:26:30 +0530

    Add files via upload

---
+++ SortingUsingCollection.java
@@ -0,0 +1,20 @@
+package week3.day2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SortingUsingCollection {
+
+	public static void main(String[] args) {
+		String[] input = {"HCL","Wipro","Aspire Systems","CTS"};
+		Set<String> setValues = new TreeSet<String>();
+		for (String temp : input) {
+			setValues.add(temp);
+			
+		}
+		
+		System.out.println(setValues);
+
+	}
+
+}
