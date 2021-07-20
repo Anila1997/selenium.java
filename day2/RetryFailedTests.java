4a1ee400b5722f64ed42d0189a35c4b6188a1f31
Anila1997 <85822563+Anila1997@users.noreply.github.com>
2021-07-20 14:03:19 +0530

    Add files via upload

---
+++ day2/RetryFailedTests.java
@@ -0,0 +1,21 @@
+package week6.day2;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryFailedTests implements IRetryAnalyzer{
+
+	int retryCount = 0;
+	
+	public boolean retry(ITestResult result) {
+		
+		if(!result.isSuccess() && retryCount < 2 ) {
+			retryCount++;
+			return true;
+		
+		}
+		
+		return false;
+	}
+
+}
---
+++ day2/TestNGTests.java
@@ -0,0 +1,70 @@
+package week6.day2;
+
+import java.util.NoSuchElementException;
+
+import org.testng.annotations.Test;
+
+public class TestNGTests {
+
+
+	@Test
+	public void createLead() {
+		System.out.println("crate lead");
+		throw new NoSuchElementException();
+		
+	}
+	
+
+	@Test
+	public void login() {
+		System.out.println("login");
+		
+	}
+	
+
+	@Test
+	public void editLead() {
+		System.out.println("editLead");
+		
+	}
+	
+	@Test
+	public void deleteLead() {
+		System.out.println("deleteLead");
+		
+	}
+	
+	
+	
+	
+
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
