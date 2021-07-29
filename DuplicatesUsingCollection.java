793a13b913527506d8c68999e150892e01a5f643
Anila1997 <85822563+Anila1997@users.noreply.github.com>
2021-07-29 18:57:16 +0530

    Add files via upload

---
+++ DuplicatesUsingCollection.java
@@ -0,0 +1,43 @@
+package week3.day2;
+
+
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class DuplicatesUsingCollection {
+
+	public static void main(String[] args) {
+		int [] data = {1,3,8,3,11,5,6,4,7,6,7};
+
+		Set<Integer> values = new LinkedHashSet<Integer>();	
+		for (Integer eachData : data) {
+			values.add(eachData);
+			
+		}
+		 System.out.println(values);
+			}
+                
+    
+	 
+
+			
+		}
+	
+
+			
+			
+		
+		
+		
+	
+
+        
+			
+		
+        	
+        
+	
+
+
